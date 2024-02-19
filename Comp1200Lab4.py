0c67dfdc9d7f49784548615699fffbcc6a1dfb94
westonlarhette <157428767+westonlarhette@users.noreply.github.com>
2024-02-19 15:33:11 -0800

    Add files via upload

---
+++ Comp1200Lab4.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 19 13:40:11 2024
+
+@author: westonlarhette
+"""
+
+"Lab 4"
+
+# Import the Numpy Library
+import numpy as np
+
+# Create a simple 1D array
+arr = np.array([1,2,3,4,5,6])
+print(arr)
+
+# Create a 2D array (matrix)
+matrix = np.array([[1,2,3],[4,5,6]])
+print(matrix)
+
+# Create a 1D array of zeros with length 4
+a = np.zeros(4)
+print(a)
+
+# Create a 3x5 sized matrix of zeros
+b = np.zeros((3,5))
+print(b)
